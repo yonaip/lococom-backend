d81491530f66fa8106ccd4718a425f9bb5de9bd9
stefko3z <svladov.dev@gmail.com>
2020-06-28 13:29:13 +0000

    change to const per default

--- src/controllers/discussion.js
+++ src/controllers/discussion.js
@@ -7,7 +7,7 @@ const config = require('../config');
 const DiscussionModel = require('../models/discussion');
 
 const createDiscussion = async (req,res) => {
-  var newDiscussion = new DiscussionModel(req.body);
+  const newDiscussion = new DiscussionModel(req.body);
 
   newDiscussion.save()
     .then(item => {
@@ -16,11 +16,11 @@ const createDiscussion = async (req,res) => {
     .catch(err => {
       res.status(400).send(`Could not create ${newDiscussion} due to ${err}`);
     });
- }
+}
 
 const getDiscussion = async (req,res) => {
   try {
-    let discussion = await DiscussionModel.findById(req.params.id).exec();
+    const discussion = await DiscussionModel.findById(req.params.id).exec();
     if (!discussion) return res.status(404).json({
         error: 'Not Found',
         message: `Discussion ${req.params.id} not found`
