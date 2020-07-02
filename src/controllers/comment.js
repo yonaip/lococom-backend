c294cb98dfb1cb907bae65d390ab331f266e9594
sylviapas <67117878+sylviapas@users.noreply.github.com>
2020-07-02 18:32:09 +0200

    Comment Endpoints
    New createComment and getComment Endpoints

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -6,27 +6,42 @@ const bcrypt = require('bcryptjs');
 const config = require('../config');
 const CommentModel = require('../models/comment');
 
-const create = async (req,res) => {
+const createComment = async (req,res) => {
   var newComment = new CommentModel(req.body);
-
-  console.log(new CommentModel(req.body));
   newComment.save()
     .then(item => {
-      res.send("Comment saved to database");
+      res.send(item);
     })
     .catch(err => {
-      res.status(400).send("unable to save Comment to database");
+      res.status(400).send(`Could not create ${newComment} due to ${err}`);
     });
  }
- const getComment = async (req,res) => {
+ /*const getComment = async (req,res) => {
   console.log (req);
   let ID = req.body.id;
   
   let comment = await CommentModel.find({ discussionId : ID }).limit(1).exec();
 
   return res.status(200).json(comment);
-}
+}*/
 
+const getComments = async (req,res) => {
+ try {
+    const comments = await CommentModel.find({discussionId: req.params.id}).exec();
+    if (!comments) return res.status(404).json({
+        error: 'Not Found',
+        message: `Comments ${req.params.id} not found`
+    });
+
+    return res.send(comments);
+  } catch(err) {
+      return res.status(500).json({
+          error: 'Internal Server Error',
+          message: err.message
+      });
+  }
+ 
+}
     
 
  
@@ -36,7 +51,7 @@ const create = async (req,res) => {
 
 
 module.exports = {
-    create,
-    getComment
+    createComment,
+    getComments
     
 };
--- src/routes/comment.js
+++ src/routes/comment.js
@@ -4,6 +4,6 @@ var router = express.Router();
 const commentController = require('../controllers/comment');
 
 // create Comment Route
-router.post('/createComment', commentController.create);
-router.post('/getComment', commentController.getComment); // we can only send a body with a post request?
+router.post('/comment', commentController.createComment);
+router.get('/comment/:id', commentController.getComments); // we can only send a body with a post request?
 module.exports = router;
