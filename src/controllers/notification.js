74638ee87f10d8ac73ef4f2b9c6a6628afa88f77
stefko3z <svladov.dev@gmail.com>
2020-07-17 22:18:17 +0000

    remove unused functions

--- src/controllers/notification.js
+++ src/controllers/notification.js
@@ -68,60 +68,6 @@ const deleteNotification = async (req,res) => {
   }
 }
 
-/** Gets all discussion
- *  TODO: refactor to use query parameters to limit search radius
- *  important for very big collections
- * @param {*} req 
- * @param {*} res 
- */
-const getAllDiscussions = async (req,res) => {
-  try {
-    const discussions = await DiscussionModel.find().exec();
-    return res.send(discussions);
-  } catch(err) {
-      return res.status(500).json({
-          error: 'Internal Server Error',
-          message: err.message
-      });
-  }
-}
-
-const upvote = async(req,res) => {
-
-  try {
-    const discussion = await DiscussionModel.findById(req.params.id).exec();
-    if (!discussion) return res.status(404).json({
-        error: 'Not Found',
-        message: `Discussion ${req.params.id} not found`
-    });
-    await DiscussionModel.updateOne({_id:req.params.id}, {$inc: {votes : 1}} )
-    return res.send(discussion);
-  } catch(err) {
-      return res.status(500).json({
-          error: 'Internal Server Error',
-          message: err.message
-      });
-  }
-}
-
-const downvote = async(req,res) => {
-
-  try {
-    const discussion = await DiscussionModel.findById(req.params.id).exec();
-    if (!discussion) return res.status(404).json({
-        error: 'Not Found',
-        message: `Discussion ${req.params.id} not found`
-    });
-    await DiscussionModel.updateOne({_id:req.params.id}, {$inc: {votes : -1}} )
-    return res.send(discussion);
-  } catch(err) {
-      return res.status(500).json({
-          error: 'Internal Server Error',
-          message: err.message
-      });
-  }
-}
-
 module.exports = {
     createNotification,
     getNotifications,
